13f4520223178248973c02bdc484e1a92775f687
Erik <43432787+ErikLager@users.noreply.github.com>
2020-02-26 12:35:43 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p id="username">Username:</p>
+        <input type="text" id="uname"></input><br />
+        <p id="password">Password:</p>
+        <input type="password" id="pword" /><br />
+        <button type="submit" onClick={Submit}>Submit</button>
       </header>
     </div>
-  );
+  )
+}
+
+const url = 'http://localhost:8080/FinalProject/api';
+
+function Submit(){
+  let uname = document.getElementById("uname").value;
+  let pword = document.getElementById("pword").value;
+  let encoded = `${btoa(`${uname}:${pword}`)}`;
+
+  // try {
+  //   fetch(url, {
+  //     method: 'POST',
+  //     headers:{
+  //       Authorization: `${btoa(`${uname}:${pword}`)}`
+  //     }
+  //   });
+  //   console.log(test);
+    
+  // } catch (error) {console.log(error);
+    
+  // }
 }
 
 export default App;
