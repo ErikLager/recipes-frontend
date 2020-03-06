d387d494038f6deabb5ac15bb8d9a24c9f2faad8
Erik <43432787+ErikLager@users.noreply.github.com>
2020-03-06 13:52:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Login from "./Login";
 import { getData } from './getrecipes';
 
-let props=0;
+let props = 0;
 
 function Footer() {
   return <footer>
@@ -13,14 +13,15 @@ function Footer() {
 }
 
 function Start() {
-  
+
   async function filter() {
     let input = document.getElementById("searchbar").value;
     console.log(input);
     const data = await getData(input);
     console.log(data);
+
   }
-  
+
 
   let currentContent;
   const [currentPage, setCurrentPage] = React.useState(0);
@@ -35,19 +36,17 @@ function Start() {
       <header className="App-header">
         {/* <button id="Log in" onClick={login}>Log in</button> */}
         <h1>E-Recipes</h1>
-        
         <h2>This site lists some recipes</h2>
         <h3>Filter here:</h3><input type="text" id="searchbar"></input><br />
-        
         <button onClick={filter}>Click here to filter</button>
       </header>
-      
+
       <Footer />
     </div>
   )
 }
 
-function login(){
+function login() {
   props.navigate(1);
 }
 
